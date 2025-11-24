520748895076c48fddd1a140ef78a70d6b763c2d
david <dawidrblx@gmail.com>
2025-11-24 22:38:39 +0100

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -2,7 +2,7 @@
 var params = new URLSearchParams(window.location.search);
 
 function sendTo(url){
-    location.href = `/FistaszjoObywatel/${url}?` + params;
+    location.href = `/FistaszjoObywatel-Fix/${url}?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
@@ -32,3 +32,4 @@ function getMobileOperatingSystem() {
   if (getMobileOperatingSystem() == 2){
       document.querySelector(".bottom_bar").style.height = "70px"
 }
+
